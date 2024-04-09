6464d78ffcc71206e9f277bf60bcd793989339b2
Lakshya Agarwal <lakshya.agarwal@metadologie.com>
2024-04-09 11:17:56 +0530

    button added

---
+++ src/app/Components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({children}) => {
+  return (
+    <button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[20px] py-[10px] '>{children}</button>
+  )
+}
+
+export default Button
