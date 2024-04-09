599f858ba2ef4c1f0d99bacf711a6fd09927582b
Lakshya Agarwal <lakshya.agarwal@metadologie.com>
2024-04-09 12:39:03 +0530

    tailwind updated

--- src/app/Components/Button.js
+++ src/app/Components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({children}) => {
   return (
-    <button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[20px] py-[10px] '>{children}</button>
+    <button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[25px] py-[10px]  '>{children}</button>
   )
 }
 
