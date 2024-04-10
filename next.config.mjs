a53c05c4d0a75963c4946157473ac1383e79ee54
Lakshya Agarwal <lakshya.agarwal@metadologie.com>
2024-04-10 15:10:41 +0530

    updated code by lakshya

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+        config.module.rules.push({
+          test: /\.(pdf)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              publicPath: '/_next/static/files',
+              outputPath: 'static/files',
+              name: '[name].[ext]',
+            },
+          },
+        });
+        return config;
+      },
+};
 
 export default nextConfig;
