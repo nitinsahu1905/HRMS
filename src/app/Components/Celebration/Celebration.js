2809303fff662d71adcc472d1cb1583010f5481c
Nitin Sahu <nitin.sahu@metadologie.com>
2024-04-17 14:17:38 +0530

    code improved for deployement

--- src/app/Components/Celebration/Celebration.js
+++ src/app/Components/Celebration/Celebration.js
@@ -1,26 +1,39 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
+import Image from "next/image";
+
+// Inside your component:
+<Image src={img} alt="Emp-img" width={24} height={24} objectFit="cover" />;
 
 const Celebration = (props) => {
-    const {name,date,occasion,img}=props;
-    // console.log(name,date,occasion,img);
-    // console.log("hello")
+  const { name, date, occasion, img } = props;
+  // console.log(name,date,occasion,img);
+  // console.log("hello")
   return (
     <div>
-       <div className="flex flex-row justify-between bg-[#f7f7f7] border-[#0684C7] border-[1px] rounded-lg w-auto h-auto mx-4 p-2">
-                    <div className="flex flex-row">
-                        <div className="overflow-hidden h-16 w-16 rounded-full border-none ">
-                            <img src={img} width={24} height={24} alt="Emp-img" class=" object-cover w-full h-full"/>
-                        </div>
-                        <div className="flex flex-col mx-2">
-                            <div className="text-black font-medium text-xl">{name}</div>
-                            <div className="text-black font-normal pt-2 text-xs">{occasion}</div>
-                        </div>
-                    </div>
-                    <div className="text-[#0684C7] font-medium flex p-5">{date}</div>
-                </div>
+      <div className="flex flex-row justify-between bg-[#f7f7f7] border-[#0684C7] border-[1px] rounded-lg w-auto h-auto mx-4 p-2">
+        <div className="flex flex-row">
+          <div className="overflow-hidden h-16 w-16 rounded-full border-none ">
+            {/* <img src={img} width={24} height={24} alt="Emp-img" class=" object-cover w-full h-full"/> */}
+            <Image
+              src={img}
+              alt="Emp-img"
+              width={24}
+              height={24}
+              objectFit="cover"
+            />
+          </div>
+          <div className="flex flex-col mx-2">
+            <div className="text-black font-medium text-xl">{name}</div>
+            <div className="text-black font-normal pt-2 text-xs">
+              {occasion}
+            </div>
+          </div>
+        </div>
+        <div className="text-[#0684C7] font-medium flex p-5">{date}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Celebration;
--- src/app/employee-management/page.js
+++ src/app/employee-management/page.js
@@ -1,22 +1,32 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import DropdownBox from '../Components/dropdownBox';
-import DropdownCheckBox from '../Components/dropdownCheckbox';
-import Table from '../Components/Table';
-import { EmployeeManagementData } from '../Constants/EmployeeManagementData';
-import Link from 'next/link';
+"use client";
+import React, { useEffect, useState } from "react";
+import DropdownBox from "../Components/dropdownBox";
+import DropdownCheckBox from "../Components/dropDownCheckbox";
+import Table from "../Components/Table";
+import { EmployeeManagementData } from "../Constants/EmployeeManagementData";
+import Link from "next/link";
 const EmployeeManagement = () => {
-  const EmployeeData = ['All Employees', 'Active Employees', 'Past Employees'];
-  const filterByData=["Gender","Age","BloodGroup","City"];
-  const sortByData=["name"];
-  const tableHeading=["Designation","Age","Gender","BloodGroup","City","DOJ"];
- 
-  const [employeeData,setEmployeeData] = useState(EmployeeManagementData);
-  const [filteredEmployeeData,setFilteredEmployeeData]=useState(EmployeeManagementData);
- 
-  const [tableHeadingList,setTableHeadingList]=useState(tableHeading);
-  const [filterTableHeading,setFilterTableHeading]=useState(tableHeadingList);
-  const [searchEmployeeName,setSearchEmployeeName]=useState("");
+  const EmployeeData = ["All Employees", "Active Employees", "Past Employees"];
+  const filterByData = ["Gender", "Age", "BloodGroup", "City"];
+  const sortByData = ["name"];
+  const tableHeading = [
+    "Designation",
+    "Age",
+    "Gender",
+    "BloodGroup",
+    "City",
+    "DOJ",
+  ];
+
+  // all states for the data management
+  const [employeeData, setEmployeeData] = useState(EmployeeManagementData);
+  const [filteredEmployeeData, setFilteredEmployeeData] = useState(
+    EmployeeManagementData
+  );
+  const [tableHeadingList, setTableHeadingList] = useState(tableHeading);
+  const [filterTableHeading, setFilterTableHeading] =
+    useState(tableHeadingList);
+  const [searchEmployeeName, setSearchEmployeeName] = useState("");
   const [selectedListValue, setSelectedListValue] = useState("");
   const [selectedFilterValue, setSelectedFilterValue] = useState([]);
   const [selectedSortValue, setSelectedSortValue] = useState("");
@@ -24,107 +34,132 @@ const EmployeeManagement = () => {
   const handleListDropdownChange = (selectedOption) => {
     setSelectedListValue(selectedOption);
 
-    if(selectedListValue==="Active Employees"){
-      const filterListData= employeeData.filter((data)=> data.status==="Active")
+    if (selectedListValue === "Active Employees") {
+      const filterListData = employeeData.filter(
+        (data) => data.status === "Active"
+      );
       setFilteredEmployeeData(filterListData);
     }
-  if(selectedListValue==="Past Employees")
-  {
-    const filterListData= employeeData.filter((data)=> data.status==="Past")
+    if (selectedListValue === "Past Employees") {
+      const filterListData = employeeData.filter(
+        (data) => data.status === "Past"
+      );
       setFilteredEmployeeData(filterListData);
-  }
-  if(selectedListValue==="All Employees"){
-    setFilteredEmployeeData(employeeData)
-    // console.log(selectedFilterValue)
-  }
- 
+    }
+    if (selectedListValue === "All Employees") {
+      setFilteredEmployeeData(employeeData);
+    }
   };
+  
   const handleFilterDropdownChange = (selectedOption) => {
-    console.log(selectedOption)
+    console.log(selectedOption);
     setSelectedFilterValue(selectedOption);
-    if(selectedFilterValue) {
+    if (selectedFilterValue) {
       // Update filterTableHeading based on selected options
-      if (selectedFilterValue.length==0) {
-      // let count=0;
-      setFilterTableHeading(tableHeadingList);
-      console.log("selected option emprty")
-      return; // Set to default headings if no options selected
-    } 
-      console.log("first time filter")
+      if (selectedFilterValue.length == 0) {
+        // let count=0;
+        setFilterTableHeading(tableHeadingList);
+        console.log("selected option emprty");
+        return; // Set to default headings if no options selected
+      }
+      console.log("first time filter");
       setFilterTableHeading(selectedFilterValue);
       return;
     }
- 
-    
   };
 
   const handleSortDropdownChange = (selectedOption) => {
     setSelectedSortValue(selectedOption);
-    if(selectedSortValue){
+    if (selectedSortValue) {
       // setEmployeeData(EmployeeManagementData);
-      const filter=filteredEmployeeData.sort((a, b) => b.id - a.id);
-      console.log(filter)
-      setFilteredEmployeeData(filter)
+      const filter = filteredEmployeeData.sort((a, b) => b.id - a.id);
+      console.log(filter);
+      setFilteredEmployeeData(filter);
     }
   };
- useEffect(()=> {handleListDropdownChange()},[selectedListValue]);
- useEffect(()=>{handleFilterDropdownChange()},[selectedFilterValue]);
- useEffect(()=>{handleSortDropdownChange()},[selectedSortValue]);
- 
+  useEffect(() => {
+    handleListDropdownChange();
+  }, [selectedListValue]);
+  useEffect(() => {
+    handleFilterDropdownChange();
+  }, [selectedFilterValue]);
+  useEffect(() => {
+    handleSortDropdownChange();
+  }, [selectedSortValue]);
+
   const searchHandler = () => {
-      //  console.log(searchEmployeeName);
-       const filterName=employeeData.filter((data)=> data.name.toLowerCase().includes(searchEmployeeName)
-      )
-      // console.log(filterName)
-       setFilteredEmployeeData(filterName);
-       }
- 
- 
+    //  console.log(searchEmployeeName);
+    const filterName = employeeData.filter((data) =>
+      data.name.toLowerCase().includes(searchEmployeeName)
+    );
+    // console.log(filterName)
+    setFilteredEmployeeData(filterName);
+  };
 
   return (
-   <div>
-     <div className="flex flex-col gap-3 pr-4">
-   
-        <div className='flex flex-row justify-between  items-center p-4   rounded-[10px]'>
-           <div className='p-3'>
-        <h1 className="text-dark-blue text-[24px] font-bold">Manage Employees</h1>
-        <p className="text-primary-blue">Dashboard / Manage Employees</p>
-           </div>
-
-           <div className='flex flex-row gap-2'>
-                  <button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[16px] py-[8px] h-full '>+ Add Employee</button>
-                  <Link href="./import-employees"><button className='bg-[#f7f7f7] rounded-[10px] px-[16px] py-[8px] border-2 border-primary-blue text-primary-blue'>Import Employees</button></Link>
+    <div>
+      <div className="flex flex-col gap-3 pr-4">
+        <div className="flex flex-row justify-between  items-center p-4   rounded-[10px]">
+          <div className="p-3">
+            <h1 className="text-dark-blue text-[24px] font-bold">
+              Manage Employees
+            </h1>
+            <p className="text-primary-blue">Dashboard / Manage Employees</p>
           </div>
 
+          <div className="flex flex-row gap-2">
+            <button className="cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[16px] py-[8px] h-full ">
+              + Add Employee
+            </button>
+            <Link href="./import-employees">
+              <button className="bg-[#f7f7f7] rounded-[10px] px-[16px] py-[8px] border-2 border-primary-blue text-primary-blue">
+                Import Employees
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className='flex justify-between items-center  bg-white rounded-[10px] space-x-4 p-4'>
-        <div className='flex flex-row gap-2 items-center'>
-               <div>
-                  <input type='text'
-                   placeholder='Enter Employee Name'
-                   maxLength={25}
-                   className='bg-[#f7f7f7] rounded-[10px] px-[16px] py-[8px] border-2 border-primary-blue outline-none '
-                   onChange={(e)=>
-                    setSearchEmployeeName(e.target.value)}/>
-               </div>
-               <button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[16px] py-[8px]  h-full ' onClick={searchHandler}>Search</button>
+        <div className="flex justify-between items-center  bg-white rounded-[10px] space-x-4 p-4">
+          <div className="flex flex-row gap-2 items-center">
+            <div>
+              <input
+                type="text"
+                placeholder="Enter Employee Name"
+                maxLength={25}
+                className="bg-[#f7f7f7] rounded-[10px] px-[16px] py-[8px] border-2 border-primary-blue outline-none "
+                onChange={(e) => setSearchEmployeeName(e.target.value)}
+              />
             </div>
+            <button
+              className="cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[16px] py-[8px]  h-full "
+              onClick={searchHandler}
+            >
+              Search
+            </button>
+          </div>
           <div>
-        <DropdownBox mainText="Employee List" Data={EmployeeData} onSelect={handleListDropdownChange}/>
+            <DropdownBox
+              mainText="Employee List"
+              Data={EmployeeData}
+              onSelect={handleListDropdownChange}
+            />
+          </div>
+          <div className="pl-1">
+            <DropdownCheckBox
+              mainText="Filter By"
+              Data={filterByData}
+              onSelect={handleFilterDropdownChange}
+            />
+          </div>
+          <div className="pl-1">
+            <DropdownBox
+              mainText=" Sort By"
+              Data={sortByData}
+              onSelect={handleSortDropdownChange}
+            />
+          </div>
+          {/* <div><button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[27px] py-[12px] h-full ' onClick={searchFilterHandler}>Search</button></div> */}
         </div>
-        <div className='pl-1' >
-        <DropdownCheckBox mainText="Filter By" Data={filterByData} onSelect={handleFilterDropdownChange}/>
-        </div>
-        <div className='pl-1' >
-        <DropdownBox mainText=" Sort By" Data={sortByData} onSelect={handleSortDropdownChange}/>
-        </div>
-        {/* <div><button className='cursor-pointer bg-button-blue-color rounded-[10px] text-white px-[27px] py-[12px] h-full ' onClick={searchFilterHandler}>Search</button></div> */}
-        </div>
-
-        
-
       </div>
-
 
       <div className="bg-white p-2 m-2">
         <Table
@@ -132,10 +167,8 @@ const EmployeeManagement = () => {
           headings={filterTableHeading}
         />
       </div>
-
     </div>
-
-    );
+  );
 };
 
 export default EmployeeManagement;
