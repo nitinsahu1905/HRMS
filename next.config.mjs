b5decf0aca33a29bc71ea9180b8dd2de087144ce
Nitin Sahu <nitin.sahu@metadologie.com>
2024-04-18 07:38:19 +0530

    specified the allowed domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,28 @@
+// next.config.js uses CommonJS syntax (module.exports), while next.config.mjs uses ECMAScript Modules (ESM) syntax (export)
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-          test: /\.(pdf)$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next/static/files',
-              outputPath: 'static/files',
-              name: '[name].[ext]',
-            },
-          },
-        });
-        return config;
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(pdf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next/static/files",
+          outputPath: "static/files",
+          name: "[name].[ext]",
+        },
       },
+    });
+    return config;
+  },
+  images: {
+    domains: [
+      "t4.ftcdn.net",
+      "designimages.appypie.com",
+      "encrypted-tbn0.gstatic.com",
+    ],
+  },
 };
 
 export default nextConfig;
